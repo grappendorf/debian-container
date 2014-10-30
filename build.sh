29a276b250319807592bc0d9afdc894dca92a48f
Dirk Grappendorf <dirk@grappendorf.net>
2014-10-30 12:10:36 +0100

    Tag as grappendorf/debian:7

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+docker rmi -f grappendorf/debian:7
 docker rmi -f grappendorf/debian:7.7
 docker build -t grappendorf/debian:7.7 .
+docker tag grappendorf/debian:7.7 grappendorf/debian:7
