0fdac716fd8144c69bba4ee1215467439ef18981
Dirk Grappendorf <dirk.grappendorf@telcat.de>
2014-11-12 15:13:33 +0100

    Remove image deletion from build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-docker rmi -f grappendorf/debian:7
-docker rmi -f grappendorf/debian:7.7
 docker build -t grappendorf/debian:7.7 .
 docker tag grappendorf/debian:7.7 grappendorf/debian:7
